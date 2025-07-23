42ba7d38708d9306ea244d9e937832a91c08e2d5
Mashukur Rahman <mashukur.rahman@g.bracu.ac.bd>
2025-07-23 20:22:12 +0600

    updated readme & prompt

--- llm_module.py
+++ llm_module.py
@@ -8,6 +8,6 @@ def ask_llm(query: str, context: str = None, model: str = "gemma3:latest") -> st
     prompt = query
     if context:
         prompt = f""" BAsed on the context provided to you please answer the user query only answer based ont he query. if the question is in bengali then answer in bengali only. Correctly format the answer. Answer like you are a terrific human assistant.
-        the Context:\n{context}\n\n and the Question: {query} now generate the answer. thank you."""
+        the Context:\n{context}\n\n and the Question: {query} now generate the answer. thank you. If the query is vague and the context is missing or not very appropriate then ask the user to ask more specific questions."""
     response = ollama.chat(model=model, messages=[{"role": "user", "content": prompt}])
     return response['message']['content'] 
